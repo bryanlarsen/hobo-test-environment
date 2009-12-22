067eb7c71a3ab3e0448b896a2c92ee94adf16f68
Bryan Larsen <bryan@larsen.st>
2009-12-21 22:05:19 -0600

    small fix to setup.sh

--- setup.sh
+++ setup.sh
@@ -11,14 +11,21 @@ rm -rf vendor/plugins/hobo
 (cd vendor/plugins; ln -s ../../../hobo .)
 rake db:migrate
 
+cd ../hobo
+git checkout origin/master
+
 cd ../agility-2.3
 git branch rails2.3 origin/rails2.3 || true
 git checkout rails2.3
 git submodule update --init
 rm -rf vendor/plugins/hobo
 (cd vendor/plugins; ln -s ../../../hobo .)
+cd ../hobo
+git checkout origin/master
+cd ../agility-2.3
 rake db:migrate
 rake db:migrate RAILS_ENV=test
+
 
 cd ../agility-jruby
 git branch jruby origin/jruby || true
@@ -26,6 +33,9 @@ git checkout jruby
 git submodule update --init
 rm -rf vendor/plugins/hobo
 (cd vendor/plugins; ln -s ../../../hobo .)
+cd ../hobo
+git checkout origin/master
+cd ../agility-jruby
 mysqladmin -u root create agility_jruby_test || true
 mysqladmin -u root create agility_jruby_development || true
 jruby -S rake db:migrate
@@ -36,6 +46,9 @@ git branch ruby1.9 origin/ruby1.9 || true
 git checkout ruby1.9
 git submodule update --init
 rm -rf vendor/plugins/hobo
+cd ../hobo
+git checkout origin/master
+cd ../agility-1.9
 (cd vendor/plugins; ln -s ../../../hobo .)
 ruby1.9.1 -S rake db:migrate
 ruby1.9.1 -S rake db:migrate RAILS_ENV=test
@@ -46,4 +59,4 @@ rake db:migrate
 rake db:migrate RAILS_ENV=test
 
 cd ../hobo
-git checkout master
+git checkout origin/master
